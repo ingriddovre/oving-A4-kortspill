35f66dec7b9c06f14913312edc5a22af403e9157
imdov <imdovre@gmail.com>
2024-03-01 12:24:43 +0100

    added isFlush method

--- src/main/java/stud/ntnu/idatt2003/oving4/cardgame/model/CheckHand.java
+++ src/main/java/stud/ntnu/idatt2003/oving4/cardgame/model/CheckHand.java
@@ -1,7 +1,19 @@
 package stud.ntnu.idatt2003.oving4.cardgame.model;
 
-public class CheckHand {
-  public static boolean isFlush(PlayingCard[] hand) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class CheckHand {
+
+  /**
+   * Checks if the hand has any distinct cards more than 1. If there are more distinct counted than 1, the method
+   * returns false, and it is therefore not a flush.
+   * @param hand ArrayList of PlayingCard Objects to check.
+   * @return Boolean value, true if it is a Flush, with all 5 suits of the hand alike, or false if not.
+   */
+  public static boolean isFlush(ArrayList<PlayingCard> hand) {
+    List<Character> handSuits = hand.stream().map(PlayingCard::getSuit).toList();
+    return handSuits.stream().distinct().count() == 1;
   }
+
 }
